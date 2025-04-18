e2a3c88d3c5798e95511b62f57b2d80327ee2213
Jayshwa <josh.young1399@gmail.com>
2025-04-18 21:37:58 +0100

    Implemented basic class and method based views to display API info.

--- BookListAPI/urls.py
+++ BookListAPI/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.books)
+    #path('books/', views.books),
+    path('books/', views.BookList.as_view()),
+    path('tested/', views.tests)
 ]
--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework import viewsets
+from rest_framework.views import APIView
 
-@api_view()
+@api_view(['POST'])
 def books(request):
     return Response('List of the books', status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
+def tests(request):
+    return Response('Test', status=status.HTTP_200_OK)
 
 # Create your views here.
+
+class BookList(APIView):
+    def get(self, request):
+        return Response({'Message':'List of the books'}, status.HTTP_200_OK)
