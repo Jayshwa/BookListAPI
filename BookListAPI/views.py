e01f6f3a81309ebe9018fe27e77ed2e5ccc04499
Jayshwa <josh.young1399@gmail.com>
2025-04-18 18:49:17 +0100

    Initial commit: Created Django Project and connected the repo

---
+++ BookListAPI/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view()
+def books(request):
+    return Response('List of the books', status=status.HTTP_200_OK)
+
+
+
+# Create your views here.
